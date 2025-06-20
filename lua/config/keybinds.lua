1aba0b5bf5bc0bce375f29a1b104bb08c21a79ea
drizlye0 <danielkastillo03@gmail.com>
2025-06-20 03:04:40 -0500

    telescope hidden files and keymaps

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 
 local map = vim.keymap.set
-local unmap = vim.keymap.del
 
 map("n", "\\", vim.cmd.Ex)
-map("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
+map("n", "dw", 'vb"_d')
