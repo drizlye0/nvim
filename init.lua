6a07a9047033458f40141896793757cd7d907a6c
drizlye0 <danielkastillo03@gmail.com>
2026-04-15 21:20:16 -0500

    adding formater

--- init.lua
+++ init.lua
@@ -1,57 +1,59 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
-vim.pack.add {
+vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
-  "https://github.com/catppuccin/nvim",
-  "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/romus204/tree-sitter-manager.nvim",
-  "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
-  "https://github.com/windwp/nvim-autopairs",
+	"https://github.com/catppuccin/nvim",
+	"https://github.com/ibhagwan/fzf-lua",
+	"https://github.com/romus204/tree-sitter-manager.nvim",
+	"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+	"https://github.com/windwp/nvim-autopairs",
 	"https://github.com/mrjones2014/smart-splits.nvim",
-  "https://github.com/windwp/nvim-ts-autotag",
-  "https://github.com/mason-org/mason.nvim",
-  "https://github.com/saghen/blink.cmp",
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/mason-org/mason-lspconfig.nvim",           -- lspconfig bridge
-  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" -- auto installer
-}
+	"https://github.com/windwp/nvim-ts-autotag",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/saghen/blink.cmp",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason-lspconfig.nvim", -- lspconfig bridge
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim", -- auto installer
+	"https://github.com/kdheepak/lazygit.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/stevearc/conform.nvim",
+})
 
 require("oil").setup()
 require("tree-sitter-manager").setup({
-  -- nohighlight = { "tsx"},
-  highlight = { "lua" , "go" },
+	-- nohighlight = { "tsx"},
+	highlight = { "lua", "go" },
 })
-require("fzf-lua").setup {
-  winopts = {
-    preview = {
-      layout = "vertical",
-    },
-
-    treesitter = {
-      enabled = false,
-    },
-  },
-
-  "hide",
-
-  file_ignore_patterns = {
-				"node_modules/",
-				".dart_tool/",
-				".idea/",
-				"build/",
-				".git",
+require("fzf-lua").setup({
+	winopts = {
+		preview = {
+			layout = "vertical",
 		},
 
-    previewers = {
-      builtin = {
-        treesitter = {
-          enabled = false,
-        },
-      },
-    }
-}
+		treesitter = {
+			enabled = false,
+		},
+	},
+
+	"hide",
+
+	file_ignore_patterns = {
+		"node_modules/",
+		".dart_tool/",
+		".idea/",
+		"build/",
+		".git",
+	},
+
+	previewers = {
+		builtin = {
+			treesitter = {
+				enabled = false,
+			},
+		},
+	},
+})
 require("fzf-lua").register_ui_select()
 require("nvim-autopairs").setup()
 require("smart-splits").setup()
@@ -59,32 +61,40 @@ require("nvim-ts-autotag").setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-tool-installer").setup({
-  ensure_installed = {
-    "lua_ls"
-  },
+	ensure_installed = {
+		"lua_ls",
+	},
 })
-require("blink.cmp").setup {
-  fuzzy =  {
-    implementation = "lua",
-  },
+require("blink.cmp").setup({
+	fuzzy = {
+		implementation = "lua",
+	},
 
-  completion = {
-    documentation = {
-      auto_show = true,
-    },
+	completion = {
+		documentation = {
+			auto_show = true,
+		},
 
-    keyword = {
-      range = "prefix",
-    },
-  },
+		keyword = {
+			range = "prefix",
+		},
+	},
 
-  keymap = {
-    preset = "enter"
-  },
-}
+	keymap = {
+		preset = "enter",
+	},
+})
 
 vim.lsp.config("*", {
-  capabilities = require("blink.cmp").get_lsp_capabilities()
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
+})
+
+require("gitsigns").setup()
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
 })
 
 vim.cmd("colorscheme catppuccin-mocha")
--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
 -- Plugins
-map("n", "\\", "<cmd>Oil<CR>", { desc = "Open parent directory" } )
+map("n", "\\", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 map("n", "<leader>ff", "<cmd>FzfLua files<CR>")
 map("n", "<leader>fw", "<cmd>FzfLua live_grep<CR>")
 
@@ -9,24 +9,38 @@ vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
 vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
 vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
 vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
-			-- moving between splits
-			vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-			vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
 
-			vim.keymap.set("t", "<C-h>", require("smart-splits").move_cursor_left)
-			vim.keymap.set("t", "<C-j>", require("smart-splits").move_cursor_down)
-			vim.keymap.set("t", "<C-k>", require("smart-splits").move_cursor_up)
-			vim.keymap.set("t", "<C-l>", require("smart-splits").move_cursor_right)
-			vim.keymap.set("t", "<C-\\>", require("smart-splits").move_cursor_previous)
+vim.keymap.set("t", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("t", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("t", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("t", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("t", "<C-\\>", require("smart-splits").move_cursor_previous)
 
-			vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-			vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-			vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-			vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
 
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
+map("n", "<leader>f", "<cmd>Format<CR>")
+map("n", "<leader>lg", "<cmd>LazyGit<CR>")
 --
 
 -- Maps
